28021a74432ff3647c80977c0486ecfc9796eb93
Nick Scheibenpflug <zorchenhimer@gmail.com>
2014-09-09 19:16:35 -0400

    Whoops.  Forgot to remove my personal tracker from the list.

--- config.php
+++ config.php
@@ -12,7 +12,6 @@ define("PATH_MODULE", PATH_ROOT . "module/");
 //Absolute path to where to store torrent files
 define("PATH_TORRENTS", PATH_ROOT . "files/");
 
-$magnet_trackers[] = "http://tracker.mrdrsr.net:6969/announce";
 $magnet_trackers[] = "udp://tracker.openbittorrent.com:80/announce";
 $magnet_trackers[] = "udp://tracker.publicbt.com:80";
 $magnet_trackers[] = "udp://tracker.ccc.de:80";

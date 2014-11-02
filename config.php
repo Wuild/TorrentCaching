bab0263ec24aca4f6acace66ed79520ec7fee7b9
Nick Scheibenpflug <zorchenhimer@gmail.com>
2014-11-02 01:20:54 -0500

    Revert "Added dynamic URI detection.  Will also detect SSL and port if non-standard ports are used."
    This reverts commit d6ef7f613c3bee3e973307d429029116bf6ffd1b.

--- config.php
+++ config.php
@@ -1,15 +1,6 @@
 <?php
 
-// Figure out the correct domain name and port.  Also check if we are using SSL.
-$_base_uri = 'http://';
-if ($_SERVER['HTTPS'] == 'on')
-        $_base_uri = 'https://';
-
-$_base_uri .= $_SERVER['HTTP_HOST'];
-if ( ($_SERVER['HTTPS'] == 'on' and $_SERVER['SERVER_PORT'] != 443) or ($_SERVER['HTTPS'] != 'on' and $_SERVER['SERVER_PORT'] != 80) )
-        $_base_uri .= ':'. $_SERVER['SERVER_PORT'];
-
-define("URL", $_base_uri. '/');
+define("URL", "http://mywebsite.com/");
 
 //Absolute path to this file
 define("PATH_ROOT", dirname(__FILE__) . "/");

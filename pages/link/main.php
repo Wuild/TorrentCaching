262b7474b872f78d2ebe1e44152748d3227c590a
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-02-02 17:35:40 -0500

    Made the link page less of an eyesore.

--- pages/link/main.php
+++ pages/link/main.php
@@ -1,8 +1,7 @@
 <div class="jumbotron">
-	<p><?php echo LINK_INFOHASH; ?></p>
-</div>
-
-<p>
+	<p style="text-align:center">To download the torrent with infohash<br />
+	<?php echo LINK_INFOHASH ?><br />
+	click one of the following links.</p>
 	<?php
 	$infohash = LINK_INFOHASH
 	if (file_exists(_configs()->paths->torrents . $infohash . '.torrent') {
@@ -10,10 +9,10 @@
 		$text_link = _configs()->website->url . 'text/' . $infohash;
 		$magnet_link = 'magnet:?xt=urn:btih:' . $infohash . '&tr=' . implode('&tr=', _configs()->trackers);
 		
-		echo "<a href=\"$torrent_link\">Torrent</a> | <a href=\"$text_link\">Text</a> | <a href=\"$magnet_link\">Magnet</a>";
+		echo "<a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$torrent_link\">Torrent</a><a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$text_link\">Text</a><a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$magnet_link\">Magnet</a>";
 	} else {
 		echo "Torrent doesn't exist.  Did you forget to upload it?";
 	}
 	
 	?>
-</p>
+</div>

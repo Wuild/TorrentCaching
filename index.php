df0151dfdab504d0de4d5a70967022828ad554a5
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-02-02 18:12:19 -0500

    Fixed missing echo statement and cleaned up the code behind the link page.

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ include("init.php");
 
 define("LINK_TYPE", isset($_GET['link_type']) ? $_GET['link_type'] : '');
 define("LINK_INFOHASH", isset($_GET['infohash']) ? $_GET['infohash'] : '');
+define("PAGE_ACTION", isset($_GET['page_action']) ? $_GET['page_action'] : "main");
 
 if (LINK_TYPE == 'link')
 	define("PAGE_URL", 'link');
 else
 	define("PAGE_URL", isset($_GET['page_url']) ? $_GET['page_url'] : "start");
-define("PAGE_ACTION", isset($_GET['page_action']) ? $_GET['page_action'] : "main");
 
 if (file_exists(_configs()->paths->torrents . LINK_INFOHASH. '.torrent') && preg_match("/^[0-F]{40}/i", LINK_INFOHASH)) {
 	if (LINK_TYPE == 'torrent' || LINK_TYPE == 'text') {
@@ -26,25 +26,9 @@ if (file_exists(_configs()->paths->torrents . LINK_INFOHASH. '.torrent') && preg
 		die($file);
 	} else {
 		$tpl = new Template(_configs()->paths->template. 'template.php');
-		$tpl->Display();
+		echo $tpl->Display();
 	}
 } else {
 	$tpl = new Template(_configs()->paths->template. 'template.php');
-	$tpl->Display();
+	echo $tpl->Display();
 }
-
-/*
-if (preg_match("/^([0-F]{40})\.torrent/i", PAGE_URL.".torrent") && file_exists(_configs()->paths->torrents . PAGE_URL.".torrent")) {
-    $file = file_get_contents(_configs()->paths->torrents . PAGE_URL.".torrent");
-	if (PAGE_ACTION == 'text') {
-		header('Content-Disposition: attachment; filename="' . PAGE_URL . '.txt"');
-		header("Content-Type: text/plain");
-	} else {
-		header('Content-Disposition: attachment; filename="' . PAGE_URL . '.torrent"');
-		header("Content-Type: application/x-bittorrent");
-	}
-    die($file);
-} else {
-    $tpl = new Template(_configs()->paths->template . "template.php");
-    echo $tpl->Display();
-}*/
--- pages/link/main.php
+++ pages/link/main.php
@@ -1,15 +1,19 @@
 <div class="jumbotron">
-	<p style="text-align:center">To download the torrent with infohash<br />
-	<?php echo LINK_INFOHASH ?><br />
-	click one of the following links.</p>
 	<?php
-	$infohash = LINK_INFOHASH
+	$infohash = LINK_INFOHASH;
 	if (file_exists(_configs()->paths->torrents . $infohash . '.torrent') {
 		$torrent_link = _configs()->website->url . $infohash;
 		$text_link = _configs()->website->url . 'text/' . $infohash;
-		$magnet_link = 'magnet:?xt=urn:btih:' . $infohash . '&tr=' . implode('&tr=', _configs()->trackers);
+		$magnet_link = 'magnet:?xt=urn:btih:' . $infohash . '&tr=' . implode('&tr=', _configs()->trackers);?>
 		
-		echo "<a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$torrent_link\">Torrent</a><a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$text_link\">Text</a><a class=\"btn btn-lg btn-success\" style=\"width:33%\" href=\"$magnet_link\">Magnet</a>";
+		<p style="text-align:center">To download the torrent with infohash<br />
+		<?php echo LINK_INFOHASH ?><br />
+		click one of the following links.</p>
+		<a class="btn btn-lg btn-success" style="width:33%" href="<?php echo $torrent_link ?>">Torrent</a>
+		<a class="btn btn-lg btn-success" style="width:33%" href="<?php echo $text_link ?>">Text</a>
+		<a class="btn btn-lg btn-success" style="width:33%" href="<?php echo $magnet_link ?>">Magnet</a>
+
+		<?php
 	} else {
 		echo "Torrent doesn't exist.  Did you forget to upload it?";
 	}
